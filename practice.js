ed0d42f34208b45dd194ca60c959d189619f4897
Joel Smith <jsmith@badinhs.org>
2025-12-29 23:57:45 -0500

    completed find average age

--- practice.js
+++ practice.js
@@ -63,3 +63,20 @@
 // alert(arr[2].name); // Pete
 
 
+// GET AVERAGE AGE
+function getAverageAge(arr){
+    let total = 0;
+    for (person in arr){
+        total += arr[person].age;
+    }
+
+    return total / arr.length;
+}
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let arr = [ john, pete, mary ];
+
+alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
